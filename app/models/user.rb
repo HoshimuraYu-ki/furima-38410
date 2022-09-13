8a840180565d9edd72534cb3c65e93c555aa4506
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-09-13 16:45:17 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :user_nickname,       presence: true
+  validates :user_first_name,     presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :user_last_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :user_first_name_kana,presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :user_last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :user_date_of_birth,  presence: true
 end
