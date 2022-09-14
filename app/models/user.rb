b8f68ee32c30e1140297c273ef2e10c504db4661
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-09-14 17:40:56 +0900

    修正#2 正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :user_nickname,       presence: true
-  validates :user_first_name,     presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :user_last_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :user_first_name,     presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+  validates :user_last_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
   validates :user_first_name_kana,presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :user_last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :user_date_of_birth,  presence: true
