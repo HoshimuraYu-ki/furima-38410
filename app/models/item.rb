b01c22473d54604c823450f2fbe0c9857cd96682
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-09-20 17:05:28 +0900

    修正#3 item_priceのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,5 +12,5 @@ class Item < ApplicationRecord
 
   validates :item_name, :item_description, :item_price, :image, presence: true
   validates :item_category_id, :item_condition_id, :item_shipping_fee_id, :item_shipped_date_id, :ship_region_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format:{ with: /\A[0-9]+\z/ }
+  validates :item_price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
