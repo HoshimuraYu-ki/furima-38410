6a73a1fa318466a52c215b15bee95577e957c397
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-09-29 10:06:51 +0900

    購入商品送付先のマイグレーションの設定

--- db/migrate/20220928095413_create_shipping_addresses.rb
+++ db/migrate/20220928095413_create_shipping_addresses.rb
@@ -1,7 +1,14 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
+      t.string  :shipping_address_postal_code
+      t.integer :ship_region_id
+      t.string  :shipping_address_city
+      t.string  :shipping_address_house_number
+      t.string  :shipping_address_building_name
+      t.string  :shipping_address_phone_number
 
+      t.references :purchase_record, foreign_key: true
       t.timestamps
     end
   end
