3259475d750661bb3abf37536a7e9b4fc81b53c0
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-10-01 18:55:27 +0900

    購入記録のコントローラーの設定

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,2 +1,30 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:create,:index]
+
+  def index
+    @order = Order.new
+  end
+
+  def new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:shipping_address_postal_code,:ship_region_id,:shipping_address_city,:shipping_address_house_number,:shipping_address_building_name,:shipping_address_phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
