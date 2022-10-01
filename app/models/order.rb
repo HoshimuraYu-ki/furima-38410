1e492d5e5557f0d08e62a2fc3a0a3ffd944b7c1f
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-10-01 19:50:06 +0900

    バリデーションの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,15 +9,23 @@ class Order
                 :shipping_address_phone_number
 
   with_options presence: true do
-    validates :shipping_address_postal_code
+    validates :user_id
+    validates :item_id
+    validates :shipping_address_postal_code, format: { with: /\A\d{3}[-]\d{4}\z/} 
     validates :ship_region_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :shipping_address_city
-    validates :shipping_address_house_number, format: { with: /\A\d{3}[-]\d{4}\z/} 
-    validates :shipping_address_phone_number, length: { maximum: 11 }
+    validates :shipping_address_house_number
+    validates :shipping_address_phone_number, format: { with: /\A\d{10,11}\z/}
   end
 
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id,item_id: item_id)
-    ShippingAddress.create(purchase_record_id: purchase_record.id,shipping_address_postal_code: shipping_address_postal_code,ship_region_id: ship_region_id,shipping_address_city: shipping_address_city,shipping_address_house_number: shipping_address_house_number,shipping_address_building_name: shipping_address_building_name,shipping_address_phone_number: shipping_address_phone_number)
+    ShippingAddress.create( purchase_record_id: purchase_record.id,
+                            shipping_address_postal_code: shipping_address_postal_code,
+                            ship_region_id: ship_region_id,
+                            shipping_address_city: shipping_address_city,
+                            shipping_address_house_number: shipping_address_house_number,
+                            shipping_address_building_name: shipping_address_building_name,
+                            shipping_address_phone_number: shipping_address_phone_number)
   end
 end
